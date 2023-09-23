b2d384b24efd4d6826c3cae607581798162e58ad
jonathan <jo.bach.s@gmail.com>
2023-09-23 11:18:55 -0300

    Script e estilo do modal

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,10 @@ function salvarUsuario(){
     email = document.getElementById("email").value;
     senha = document.getElementById("senha").value;
     localStorage.setItem(nome, '{"nome":"'+ nome +'", "email":"'+email+'", "senha":"'+senha+'"}')
-    alert("O Usuário "+ nome +" foi salvo com sucesso!");
     console.log(localStorage.getItem(nome))
+    window.open('#modal-cadastro', '_self')
+}
+
+function fechar(){
+    window.open('#', '_self')
 }
