f6ab4504b1bdab95a0d610be12c8eed24b383c3d
jonathan <jo.bach.s@gmail.com>
2023-10-07 19:13:44 -0300

    Finalizando app

--- javascript/script.js
+++ javascript/script.js
@@ -8,13 +8,62 @@ function salvarUsuario(){
 }
 
 function salvarNaAgenda(){
+    dataId = new Date();
     nome = document.getElementById("nome").value;
     data = document.getElementById("data").value;
     hora = document.getElementById("hora").value;
     treino = document.getElementById("treino").value;
-    localStorage.setItem(nome+data, '{"nome":"'+ nome +'", "data":"'+data+'", "hora":"'+hora+'", "treino":"'+treino+'"}')
-    console.log(localStorage.getItem(nome))
+    
+    agenda = localStorage.getItem('agenda');
+    if (!agenda){
+        agenda = '[]';
+    }
+
+    const obj = JSON.parse(agenda);
+    obj.push({
+        dataId,
+        nome,
+        data,
+        hora,
+        treino        
+    })
+
+
+    localStorage.setItem('agenda', JSON.stringify(obj));
     window.open('#modal-cadastro', '_self')
+}
+
+function getDadosAgenda(){
+    console.log('foi get dados');
+    tbody = document.getElementsByTagName('tbody')[0];
+
+    agenda = localStorage.getItem('agenda');
+    if (!agenda){
+        agenda = '[]';
+    }
+
+    console.log(agenda);
+    console.log(tbody);
+
+    const obj = JSON.parse(agenda);
+
+    for (let index = 0; index < obj.length; index++){
+        if (tbody.innerHTML){
+            tbody.innerHTML += `<tr>
+                <td>${obj[index].nome}</td>
+                <td>${obj[index].data}</td>
+                <td>${obj[index].hora}</td>
+                <td>${obj[index].treino}</td>
+            </tr>`
+        } else {
+            tbody.innerHTML = `<tr>
+                <td>${obj[index].nome}</td>
+                <td>${obj[index].data}</td>
+                <td>${obj[index].hora}</td>
+                <td>${obj[index].treino}</td>
+            </tr>`
+        }
+    }
 }
 
 function fechar(){
